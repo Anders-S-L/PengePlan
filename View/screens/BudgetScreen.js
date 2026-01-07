f5b3d32278d0e60af22258e1124ce40968223ba9
ASL <s233954@dtu.dk>
2026-01-07 13:39:06 +0100

    aa

--- View/screens/BudgetScreen.js
+++ View/screens/BudgetScreen.js
@@ -1,12 +1,15 @@
 // View/screens/BudgetScreen.js
-import React from "react";
-import { View, Text } from "react-native";
-
+import React, { useState } from "react";
+import { View, Text, TextInput } from "react-native";
+import { PrimaryAddButton } from "../PrimaryAddButtons";
 import { useBudgetViewModel } from "../../ViewModel/Budget/useBudgetViewModel";
 import { TotalsView } from "../TotalsView";
 
+
 export function BudgetScreen() {
     const vm = useBudgetViewModel();
+    const [name, setName] = useState("");
+    const [amount, setAmount] = useState("");
 
     if (vm.isLoading) return <Text>Indlæser...</Text>;
     if (!vm.budget) return <Text>Ingen budget endnu</Text>;
@@ -17,15 +20,48 @@ export function BudgetScreen() {
         { name: "Rådighedsbeløb", value: vm.disposable },
     ];
 
+    async function addFixedIncome() {
+        if (!name || !amount) return;
+        await vm.addFixedIncome({ name, amount: parseFloat(amount) });
+        setName("");
+        setAmount("");
+    }
+
+    async function addFixedExpense() {
+        if (!name || !amount) return;
+        await vm.addFixedExpense({ name, amount: parseFloat(amount) });
+        setName("");
+        setAmount("");
+    }
+
     return (
-        <View style={{ padding: 16, marginTop: 50 }}>
-            <Text style={{ fontSize: 24, fontWeight: "700", alignContent: "center" }}>
+        <View style={{ padding: 16 }}>
+            <Text style={{ fontSize: 24, fontWeight: "700", marginBottom: 30, marginTop: 50 }}>
                 Velkommen til PengePlan
             </Text>
 
-            <View style={{ marginTop: 16 }}>
-                <TotalsView totals={items} />
-            </View>
+            <TotalsView totals={items} />
+
+            <Text style={{ marginTop: 16 }}>Navn</Text>
+            <TextInput
+                value={name}
+                onChangeText={setName}
+                placeholder="Løn / Husleje"
+                style={{ borderWidth: 1, padding: 10, borderRadius: 8, marginTop: 15 }}
+            />
+
+            <Text style={{ marginTop: 12 }}>Beløb</Text>
+            <TextInput
+                value={amount}
+                onChangeText={setAmount}
+                placeholder="fx 12000"
+                keyboardType="numeric"
+                style={{ borderWidth: 1, padding: 10, borderRadius: 8, marginTop: 6 }}
+            />
+
+            <PrimaryAddButton title="Tilføj indtægt" onPress={addFixedIncome} />
+            <PrimaryAddButton title="Tilføj udgift" onPress={addFixedExpense} />
         </View>
     );
 }
+
